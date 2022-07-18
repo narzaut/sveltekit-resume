260730e675853a27bd5e88aee2382071c4d0f79c
Ignacio Arzaut <ignacio.arzaut@surfdevelopment.tech>
2022-07-18 13:12:12 -0300

    adjust animations

--- src/components/tech.bar.svelte
+++ src/components/tech.bar.svelte
@@ -25,7 +25,7 @@
   onMount(() => animation())
 </script>
 
-<div id={`techbar/${id}`} in:fly={{x:-200, duration:1200, delay:200 * index}} class=''>
+<div id={`techbar/${id}`} in:fly={{x:-1000, duration:800, delay:150 * index}} class=''>
   <p class='py-1 text-sm lg:text-lg text-gray font-mono font-semibold'>{name}</p>
   <div class='w-full h-10 rounded bg-secondary '>
     <div class='h-full flex items-center pl-2 rounded bg-gradient-to-r from-lighter-green to-green border-l-4 border-green' style={`width: ${percent-multiplier}%`}; >
