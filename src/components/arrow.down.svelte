c580386319da7c6826511423477bea927ae88b72
Ignacio Arzaut <ignacio.arzaut@surfdevelopment.tech>
2022-07-18 06:47:25 -0300

    fix arrowdown

--- src/components/arrow.down.svelte
+++ src/components/arrow.down.svelte
@@ -5,6 +5,6 @@
 </script>
 <div class='absolute right-0 left-0 bottom-0 fadeInDown'>
   <a href={`#${href}`}> 
-    <ArrowDownCircleIcon size='2.5x'class="relative bottom-5 lg:bottom-5 text-gray inline animate-bounce cursor-pointer hover-text-green-400 transition-colors"/>
+    <ArrowDownCircleIcon size='2.5x'class="relative bottom-12 lg:bottom-5 text-gray inline animate-bounce cursor-pointer hover-text-green-400 transition-colors"/>
   </a>
 </div>
