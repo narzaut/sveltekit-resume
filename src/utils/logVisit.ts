cf45c4a2c255010abc004ea8c9ab7309008db667
Ignacio Arzaut <ignacio.arzaut@surfdevelopment.tech>
2023-02-03 13:16:57 -0300

    smol fix

--- src/utils/logVisit.ts
+++ src/utils/logVisit.ts
@@ -1,15 +1,8 @@
-let startTime: Date;
-let endTime: Date;
-
 async function handleBeforeUnload() {
-  endTime = new Date();
-  let startTimestamp = startTime.getTime();
-  let endTimestamp = endTime.getTime();
-  let duration = endTimestamp - startTimestamp;
-  fetch('https://getdata-narzaut.vercel.app/api/getdata');
+
+  fetch("https://getdata-narzaut.vercel.app/api/send-log")
 }
 
 export default function logVisit() {
-  startTime = new Date();
   window.addEventListener('beforeunload', handleBeforeUnload);
 }
